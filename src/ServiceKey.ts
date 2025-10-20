ddb3cf4d7bb9943709ca2e7dbef38d814754acf7
Juan Herrera <juanhr454@gmail.com>
2025-10-20 09:40:15 -0300

    Make `symbol` property public in `ServiceKey` class

--- src/ServiceKey.ts
+++ src/ServiceKey.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 export class ServiceKey<T> {
-  private readonly symbol: symbol;
+  public readonly symbol: symbol;
 
   constructor(public readonly name: string) {
     this.symbol = Symbol(name);
