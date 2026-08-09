cdcac12486347c5ad24f7aa66254b9867391dfb5
Juan Herrera <juanhr454@gmail.com>
2026-08-08 22:28:27 -0300

    refactor(core): rename `ServiceType` to `DependencyType` for improved clarity in type definitions

--- packages/core/src/serviceFactory.ts
+++ packages/core/src/serviceFactory.ts
@@ -5,7 +5,7 @@ import {
 } from './errors'
 import type { Selector } from './serviceSelector'
 
-type ServiceType<T> =
+type DependencyType<T> =
   T extends SelectorKey<infer U>
     ? Selector<U>
     : T extends ServiceKey<infer U>
@@ -14,7 +14,7 @@ type ServiceType<T> =
 
 // Helper types to convert an array/tuple of ServiceKey to tuple of their types
 type DependencyTypes<T extends readonly ServiceKey<unknown>[]> = {
-  [K in keyof T]: ServiceType<T[K]>
+  [K in keyof T]: DependencyType<T[K]>
 }
 
 /**
